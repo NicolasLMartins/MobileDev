62b6d83ab7c16f0f555020ac4696920b77b119d6
NicolasLMartins <116754178+NicolasLMartins@users.noreply.github.com>
2024-03-20 20:43:55 -0300

    UPDATE Calculadora (C)

--- aula14032024/Calculadora/dist/Main.js
+++ aula14032024/Calculadora/dist/Main.js
@@ -1,55 +1,42 @@
+"use strict";
 class Calculadora {
-    static adicao(...values: number[]): number {
-        let sum: number = 0;
-
+    static adicao(...values) {
+        let sum = 0;
         values.forEach(value => {
             sum += value;
         });
-
         return sum;
     }
-
-    static subtracao(...values: number[]): number {
-        let result: number = values[0];
-
+    static subtracao(...values) {
+        let result = values[0];
         values.forEach((value, i) => {
             if (i !== 0) {
                 result -= value;
             }
         });
-
         return result;
     }
-
-    static multiplicacao(...values: number[]): number {
+    static multiplicacao(...values) {
         let product = 1;
-
         values.forEach(value => {
             product *= value;
         });
-
         return product;
     }
-
-    static divisao(...values: number[]): number {
+    static divisao(...values) {
         if (values.some(value => value === 0)) {
             console.log("ERRO: Divisão por 0.");
         }
-
         let result = values[0];
-
         values.forEach((value, i) => {
             if (i !== 0) {
                 result /= value;
             }
         });
-        
         return result;
     }
 }
-
-const values: number[] = [2, 4, 6];
-
+const values = [2, 4, 6];
 console.log(Calculadora.adicao(...values));
 console.log(Calculadora.subtracao(...values));
 console.log(Calculadora.multiplicacao(...values));
