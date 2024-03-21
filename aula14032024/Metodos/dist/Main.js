08cba6b8c1ebf72ea0ca945bc3e2957d368a7d6a
NicolasLMartins <116754178+NicolasLMartins@users.noreply.github.com>
2024-03-20 21:26:52 -0300

    UPDATE Metodos (C)

--- aula14032024/Metodos/dist/Main.js
+++ aula14032024/Metodos/dist/Main.js
@@ -1,26 +1,18 @@
+"use strict";
 class Metodos {
-    private nome: string;
-    
-    constructor(
-        nome: string
-    ) {
-        /* CONSTRUTOR */ 
+    constructor(nome) {
+        /* CONSTRUTOR */
         this.nome = nome; // _nome que é privado recebe o valor de nome que é público
     }
-
-    public cadastra(): string {
+    cadastra() {
         return this.nome;
     }
-
-    public toString(): string {
+    toString() {
         return `Aluno cadastrado: ${this.nome}`;
     }
 }
-
 // "João" é passado por parâmetro para o método constructor()
 // da classe Metodos e instancia um novo objeto
 const metodo = new Metodos("João") /* OBJETO */;
-
 metodo.cadastra();
-
 console.log(metodo.toString());
