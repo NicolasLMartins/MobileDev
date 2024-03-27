f4e5841799ee773ed15130c2b6bc99082452c01a
Colégio Concórdia <vs2020concordia@hotmail.com>
2024-03-27 16:35:06 -0300

    ADD Locadora (C)

---
+++ aula20032024/operadoresAritmeticosEMetodos/dist/Locadora.js
@@ -0,0 +1,45 @@
+"use strict";
+var ModeloCarro;
+(function (ModeloCarro) {
+    ModeloCarro["TOYOTA_COROLLA"] = "Toyota Corolla";
+    ModeloCarro["VOLKSWAGEN_JETTA"] = "Volkswagen Jetta";
+    ModeloCarro["FIAT_UNO"] = "Fiat Uno";
+})(ModeloCarro || (ModeloCarro = {}));
+class Locadora {
+    constructor() {
+        this._valoresDiarios = {
+            [`${ModeloCarro.TOYOTA_COROLLA}_2024`]: 150000,
+            [`${ModeloCarro.VOLKSWAGEN_JETTA}_2016`]: 70000,
+            [`${ModeloCarro.FIAT_UNO}_1999`]: 40000,
+        };
+    }
+    alugarCarro(modelo, ano, qtdDias) {
+        const chave = `${modelo}_${ano}`;
+        if (!(chave in this._valoresDiarios)) {
+            console.log(`O ano ${ano} não está cadastrado para o modelo ${modelo}`);
+            return;
+        }
+        this._modeloCarro = modelo;
+        this._anoCarro = ano;
+        this._qtdDias = qtdDias;
+        this._valorTotal = this.valorTotal();
+    }
+    ;
+    valorTotal() {
+        if (this._valoresDiarios) {
+        }
+        const chave = `${this._modeloCarro}_${this._anoCarro}`;
+        const valorDiario = this._valoresDiarios[chave];
+        return this._qtdDias * valorDiario;
+    }
+    imprimirInformacoes() {
+        console.log(`Modelo e ano do carro alugado: ${this._modeloCarro}, ${this._anoCarro}. Valor total: R$ ${this._valorTotal.toFixed(2)}`);
+    }
+}
+const locadora = new Locadora();
+locadora.alugarCarro(ModeloCarro.TOYOTA_COROLLA, 2024, 2);
+locadora.imprimirInformacoes();
+locadora.alugarCarro(ModeloCarro.VOLKSWAGEN_JETTA, 2016, 2);
+locadora.imprimirInformacoes();
+locadora.alugarCarro(ModeloCarro.FIAT_UNO, 1999, 2);
+locadora.imprimirInformacoes();
---
+++ aula20032024/operadoresAritmeticosEMetodos/src/Locadora.ts
@@ -0,0 +1,65 @@
+// Enunciado: Crie uma classe chamada locadora que recebe valores nas variáveis: modelo-carro, ano-carro,
+// valor-diaria, qtd-dias, valor-total.
+// Faça um método que imprima na tela o modelo-carro, ano-carro e o valor-total.
+interface ValorDiario {
+    [modeloAno: string]: number;
+}
+
+enum ModeloCarro {
+    TOYOTA_COROLLA = "Toyota Corolla",
+    VOLKSWAGEN_JETTA = "Volkswagen Jetta",
+    FIAT_UNO = "Fiat Uno",
+}
+
+class Locadora {
+    private _modeloCarro!: string;
+    private _anoCarro!: number;
+    private _qtdDias!: number;
+    private _valoresDiarios: ValorDiario;
+    private _valorTotal!: number;
+
+    constructor() {
+        this._valoresDiarios = {
+            [`${ModeloCarro.TOYOTA_COROLLA}_2024`]: 150000,
+            [`${ModeloCarro.VOLKSWAGEN_JETTA}_2016`]: 70000,
+            [`${ModeloCarro.FIAT_UNO}_1999`]: 40000,
+        };
+    }
+
+    public alugarCarro(modelo: ModeloCarro, ano: number, qtdDias: number): void {
+        const chave = `${modelo}_${ano}`;
+        if (!(chave in this._valoresDiarios)) {
+            console.log(`O ano ${ano} não está cadastrado para o modelo ${modelo}`);
+            return;
+        }
+
+        this._modeloCarro = modelo;
+        this._anoCarro = ano;
+        this._qtdDias = qtdDias;
+        this._valorTotal = this.valorTotal();
+    };
+
+    private valorTotal(): number {
+        if (this._valoresDiarios) {
+
+        }
+        const chave = `${this._modeloCarro}_${this._anoCarro}`
+        const valorDiario = this._valoresDiarios[chave];
+        return this._qtdDias * valorDiario;
+    }
+
+    public imprimirInformacoes(): void {
+        console.log(`Modelo e ano do carro alugado: ${this._modeloCarro}, ${this._anoCarro}. Valor total: R$ ${this._valorTotal.toFixed(2)}`);
+    }
+}
+
+const locadora = new Locadora();
+
+locadora.alugarCarro(ModeloCarro.TOYOTA_COROLLA, 2024, 2);
+locadora.imprimirInformacoes();
+
+locadora.alugarCarro(ModeloCarro.VOLKSWAGEN_JETTA, 2016, 2);
+locadora.imprimirInformacoes();
+
+locadora.alugarCarro(ModeloCarro.FIAT_UNO, 1999, 2);
+locadora.imprimirInformacoes();
