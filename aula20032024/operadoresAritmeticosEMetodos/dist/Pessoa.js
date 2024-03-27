5f88d605e3e47559b6a15b05d940b38c57ed5ca6
Colégio Concórdia <vs2020concordia@hotmail.com>
2024-03-27 16:35:28 -0300

    ADD Pessoa (C)

---
+++ aula20032024/operadoresAritmeticosEMetodos/dist/Pessoa.js
@@ -0,0 +1,23 @@
+"use strict";
+// Enunciado: Crie uma classe chamada Pessoa que tenha as propriedades nome e idade,
+// e um método que retorne uma mensagem com o nome e a idade da pessoa.
+class Pessoa {
+    constructor(nome, idade) {
+        this._nome = nome;
+        this._idade = idade;
+    }
+    toString() {
+        return `Nome: ${this._nome}, Idade: ${this._idade}`;
+    }
+    set nome(novoNome) {
+        this._nome = novoNome;
+    }
+    set idade(novaIdade) {
+        this._idade = novaIdade;
+    }
+}
+const pessoa = new Pessoa("Eduardo", 18);
+console.log(pessoa.toString());
+pessoa.nome = "Larah";
+pessoa.idade = 17;
+console.log(pessoa.toString());
