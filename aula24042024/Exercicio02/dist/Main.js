b93a906fb61a3344eddc56678297e62c7a0a7b8f
NicolasLMartins <nicolaslm2006@gmail.com>
2024-04-24 14:16:44 -0300

    ADD Exercicio02 (C)

---
+++ aula24042024/Exercicio02/dist/Main.js
@@ -0,0 +1,5 @@
+"use strict";
+for (let i = 0; i < 10; i++) {
+    let num = Math.ceil(Math.random() * 100);
+    num % 2 === 0 ? console.log(num) : i--;
+}
---
+++ aula24042024/Exercicio02/dist/types/classes/Numeros.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Numeros = void 0;
+class Numeros {
+    static imprimirNumeros(numTuple) {
+        numTuple.forEach(num => {
+            console.log(num);
+        });
+    }
+}
+exports.Numeros = Numeros;
