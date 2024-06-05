a5422682bbaba0318e8fdf6b32cacae2d634aee2
NicolasLMartins <nicolaslm2006@gmail.com>
2024-06-05 17:00:35 -0300

    UPDATE App pages

--- aula05062024/AppSidemenu/src/app/app.component.ts
+++ aula05062024/AppSidemenu/src/app/app.component.ts
@@ -6,13 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Home', url: '/Home', icon: 'home' },
+    { title: 'Empresa', url: '/Empresa', icon: 'bag' },
+    { title: 'Produtos', url: '/Produtos', icon: 'cart' },
+    { title: 'Contato', url: '/Contato', icon: 'call' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
